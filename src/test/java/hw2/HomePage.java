7d06bf27a9d87593fbacb604322ac7d2b02c4a4b
Robert Andreev <andreev.robert.a@gmail.com>
2021-03-14 00:18:03 +0300

    Add get for navigation in header

--- src/test/java/hw2/HomePage.java
+++ src/test/java/hw2/HomePage.java
@@ -1,9 +1,12 @@
 package hw2;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class HomePage {
     /**
@@ -33,6 +36,9 @@ public class HomePage {
 
     @FindBy(id = "user-name")
     private WebElement name;
+
+    @FindBy(css = "ul.nav>li>a")
+    private List<WebElement> navigation;
 
     /**
      * метод для ввода логина
@@ -74,5 +80,12 @@ public class HomePage {
      */
     public String getName() {
         return name.getAttribute("innerText");
+    }
+
+    /**
+     * метод для нахождения 4-х навигационных элементов в хедере
+     */
+    public List<WebElement> getHeaderSectionNavigation() {
+        return navigation;
     }
 }
