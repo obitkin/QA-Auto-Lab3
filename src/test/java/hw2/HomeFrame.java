a4ed350ac7e87e6f181cc3bc0cbed90f4ed26e96
Robert Andreev <andreev.robert.a@gmail.com>
2021-03-14 14:01:55 +0300

    Add frame page object

---
+++ src/test/java/hw2/HomeFrame.java
@@ -0,0 +1,28 @@
+package hw2;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomeFrame {
+    /**
+     * конструктор класса, занимающийся инициализацией полей класса
+     */
+    public WebDriver driver;
+
+    public HomeFrame(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    @FindBy(css = "div.epam-logo")
+    private WebElement logo;
+
+    /**
+     * метод для нахождения иконки EPAM
+     */
+    public WebElement getLogo() {
+        return logo;
+    }
+}
