cb1f45e24a5d781c7978b744a7b1567d7a01ec9c
Robert Andreev <andreev.robert.a@gmail.com>
2021-03-21 00:05:21 +0300

    Fix test 5 and 6; Add softAsserts

--- src/test/java/hw2/ex2/Ex2Test.java
+++ src/test/java/hw2/ex2/Ex2Test.java
@@ -13,15 +13,17 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 
+import java.awt.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Ex2Test {
 
-    String url = "https://jdi-testing.github.io/jdi-light/index.html";
-
+    String URL = "https://jdi-testing.github.io/jdi-light/index.html";
+    String diffURL = "https://jdi-testing.github.io/jdi-light/different-elements.html";
     List<String> serviceList = Arrays.asList("Support", "Dates", "Complex Table", "Simple Table", "Tables With Pages", "Different Elements");
 
     WebDriver driver;
@@ -40,7 +42,7 @@ public class Ex2Test {
         loginTest = new LoginTest(homePage);
 
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
     }
 
     /**
@@ -48,8 +50,8 @@ public class Ex2Test {
      */
     @Test(priority = 1)
     public void openSiteTest() {
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(),url);
+        driver.get(URL);
+        Assert.assertEquals(driver.getCurrentUrl(),URL);
     }
 
     /**
@@ -80,39 +82,40 @@ public class Ex2Test {
      *  5. Click on "Service" subcategory in the header and check that drop down contains options
      */
     @Test(priority = 5)
-    public void assertServiceInHeader() {
+    public void assertHeaderService() {
         WebElement service = homePage.header.getNavigationElement("Service");
         service.click();
-        serviceList
-                .forEach(
-                        x -> Assert.assertTrue(
-                                homePage.header
-                                        .takeElementsFromList(service)
-                                        .stream()
-                                        .map(WebElement::getText)
-                                        .collect(Collectors.toList())
-                                        .contains(x)
-                        )
-                );
+        List<String> foundElements = homePage.header.takeElementsFromList(service)
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+
+        SoftAssert softAssertion = new SoftAssert();
+        for (String elem : serviceList) {
+            softAssertion.assertTrue(foundElements.stream().anyMatch(elem::equalsIgnoreCase),
+                    elem + " NOT FOUND");
+        }
+        softAssertion.assertAll();
     }
 
     /**
      *  6. Click on Service subcategory in the left section and check that drop down contains options
      */
     @Test(priority = 6)
-    public void ServiceOnLeft() {
-        homePage.getServiceLeft().click();
-        serviceList
-                .forEach(
-                        x -> Assert.assertTrue(
-                                homePage
-                                        .getServiceListLeft()
-                                        .stream()
-                                        .map(WebElement::getText)
-                                        .collect(Collectors.toList())
-                                        .contains(x)
-                        )
-                );
+    public void assertLeftService() {
+        WebElement service = homePage.leftSection.getNavigationElement("Service");
+        service.click();
+        List<String> foundElements = homePage.leftSection.takeElementsFromList(service)
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
+
+        SoftAssert softAssertion = new SoftAssert();
+        for (String elem : serviceList) {
+            softAssertion.assertTrue(foundElements.stream().anyMatch(elem::equalsIgnoreCase),
+                    elem + " NOT FOUND");
+        }
+        softAssertion.assertAll();
     }
 
     /**
@@ -125,7 +128,7 @@ public class Ex2Test {
                 "Different elements",
                 driver,
                 DifferentElementsPage::new);
-        Assert.assertEquals(driver.getCurrentUrl(),"https://jdi-testing.github.io/jdi-light/different-elements.html");
+        Assert.assertEquals(driver.getCurrentUrl(),diffURL);
     }
 
     /**
@@ -133,20 +136,20 @@ public class Ex2Test {
      */
     @Test(priority = 8, timeOut = 10000)
     public void checkMain() {
-        Assert.assertEquals(diffPage.main.getRadio().size(),4);
-        Assert.assertEquals(diffPage.main.getCheckBoxes().size(),4);
-        Assert.assertEquals(diffPage.main.getButtons().size(),2);
-        Assert.assertEquals(diffPage.main.getDropDown().size(),1);
-        //diffPage.main.getRadio().get(0).click();
-        //Assert.assertTrue(diffPage.main.getRadio().get(0).isSelected());
+        SoftAssert softAssertion = new SoftAssert();
+        softAssertion.assertEquals(diffPage.main.getRadio().size(),4);
+        softAssertion.assertEquals(diffPage.main.getCheckBoxes().size(),4);
+        softAssertion.assertEquals(diffPage.main.getButtons().size(),2);
+        softAssertion.assertEquals(diffPage.main.getDropDown().size(),1);
+        softAssertion.assertAll();
     }
 
     /**
      *  9. Assert that there is Right Section
      */
-    @Test(priority = 9, timeOut = 1000)
+    @Test(priority = 9, timeOut = 500)
     public void assertRightSection() {
-        Assert.assertNotNull(diffPage.rightSection);
+        diffPage.rightSection.checkSelf();
     }
 
     /**
@@ -154,7 +157,7 @@ public class Ex2Test {
      */
     @Test(priority = 10, timeOut = 1000)
     public void assertLeftSection() {
-        Assert.assertNotNull(diffPage.leftSection);
+        diffPage.leftSection.checkSelf();
     }
 
     /**
@@ -162,10 +165,12 @@ public class Ex2Test {
      */
     @Test(priority = 11)
     public void selectCheckboxes() {
-        diffPage.main.getCheckBoxes("Water").click();
-        diffPage.main.getCheckBoxes("Wind").click();
-        Assert.assertTrue(diffPage.main.getCheckBoxes("Water").isSelected());
-        Assert.assertTrue(diffPage.main.getCheckBoxes("Wind").isSelected());
+        SoftAssert softAssertion = new SoftAssert();
+        diffPage.main.getCheckBoxes("Water").select();
+        diffPage.main.getCheckBoxes("Wind").select();
+        softAssertion.assertTrue(diffPage.main.getCheckBoxes("Water").isSelected());
+        softAssertion.assertTrue(diffPage.main.getCheckBoxes("Wind").isSelected());
+        softAssertion.assertAll();
     }
 
     /**
@@ -175,19 +180,21 @@ public class Ex2Test {
     @Test(priority = 12)
     public void assertCheckbox() {
         diffPage.main.clear();
+        SoftAssert softAssertion = new SoftAssert();
         for (WebElement checkBox : diffPage.main.getCheckBoxes()) {
             checkBox.click();
-            Assert.assertTrue(Main.getInputFromLabel(checkBox).isSelected());
-            Assert.assertTrue(
+            softAssertion.assertTrue(Main.getInput(checkBox).isSelected());
+            softAssertion.assertTrue(
                     diffPage.rightSection.getLog().get(0).getText().contains(checkBox.getText()) &&
                             diffPage.rightSection.getLog().get(0).getText().contains("true")
                     );
             checkBox.click();
-            Assert.assertFalse(Main.getInputFromLabel(checkBox).isSelected());
-            Assert.assertTrue(
+            softAssertion.assertFalse(Main.getInput(checkBox).isSelected());
+            softAssertion.assertTrue(
                     diffPage.rightSection.getLog().get(0).getText().contains(checkBox.getText()) &&
                             diffPage.rightSection.getLog().get(0).getText().contains("false")
             );
+            softAssertion.assertAll();
         }
     }
 
@@ -206,11 +213,13 @@ public class Ex2Test {
     @Test(priority = 14)
     public void assertRadio() {
         diffPage.main.clear();
+        SoftAssert softAssertion = new SoftAssert();
         for (WebElement radio : diffPage.main.getRadio()) {
             radio.click();
-            Assert.assertTrue(Main.getInputFromLabel(radio).isSelected());
-            Assert.assertTrue(diffPage.rightSection.getLog().get(0).getText().contains(radio.getText()));
+            softAssertion.assertTrue(Main.getInput(radio).isSelected());
+            softAssertion.assertTrue(diffPage.rightSection.getLog().get(0).getText().contains(radio.getText()));
         }
+        softAssertion.assertAll();
     }
 
     /**
@@ -227,10 +236,12 @@ public class Ex2Test {
      */
     @Test(priority = 16)
     public void assertDropDown() {
+        SoftAssert softAssertion = new SoftAssert();
         for (WebElement option : diffPage.main.getDropDown().get(0).getOptions()) {
             diffPage.main.getDropDown().get(0).selectByVisibleText(option.getText());
-            Assert.assertTrue(diffPage.rightSection.getLog().get(0).getText().contains(option.getText()));
+            softAssertion.assertTrue(diffPage.rightSection.getLog().get(0).getText().contains(option.getText()));
         }
+        softAssertion.assertAll();
     }
 
     /**
