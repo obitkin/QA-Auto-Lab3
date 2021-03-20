3e93e3500853a6d70c27ae8952c2aef6ac38d7ea
Robert Andreev <andreev.robert.a@gmail.com>
2021-03-20 17:49:29 +0300

    Add part(main, left, right) of DiffPage in DiffPage

--- src/main/java/hw2/pages/DifferentElementsPage.java
+++ src/main/java/hw2/pages/DifferentElementsPage.java
@@ -1,5 +1,9 @@
 package hw2.pages;
 
+import hw2.inner.Header;
+import hw2.inner.LeftSection;
+import hw2.inner.Main;
+import hw2.inner.RightSection;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -8,13 +12,16 @@ import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementDecorator;
 import ru.yandex.qatools.htmlelements.loader.decorator.HtmlElementLocatorFactory;
 
 public class DifferentElementsPage {
-    /**
-     * конструктор класса, занимающийся инициализацией полей класса
-     */
+
     public WebDriver driver;
+
+    public Main main;
+    public LeftSection leftSection;
+    public RightSection rightSection;
 
     public DifferentElementsPage(WebDriver driver) {
         PageFactory.initElements(new HtmlElementDecorator(new HtmlElementLocatorFactory(driver)),this);
         this.driver = driver;
     }
+
 }
