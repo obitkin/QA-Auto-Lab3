187adf0a8c23417ccc95d1cc00d31781d261692a
Robert Andreev <andreev.robert.a@gmail.com>
2021-03-20 20:34:34 +0300

    Add 2 panel

--- src/main/java/hw2/inner/RightSection.java
+++ src/main/java/hw2/inner/RightSection.java
@@ -1,9 +1,25 @@
 package hw2.inner;
 
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import ru.yandex.qatools.htmlelements.element.HtmlElement;
 
-@FindBy(xpath = "/section[@class=\"uui-info-panel-horizontal\"]/..")
+import java.util.List;
+
+@FindBy(xpath = "//section[@class=\"uui-info-panel-horizontal\"]/..")
 public class RightSection extends HtmlElement {
 
+    @FindBy(css = ".info-panel-body-log li")
+    private List<WebElement> log;
+
+    @FindBy(css = ".info-panel-body-result li")
+    private List<WebElement> result;
+
+    public List<WebElement> getLog() {
+        return log;
+    }
+
+    public List<WebElement> getResult() {
+        return result;
+    }
 }
